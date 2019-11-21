b2a80b8aadabd8e29d94e81dfaccfd03f5bfe399
ThomasBEce <58028659+ThomasBEce@users.noreply.github.com>
2019-11-21 10:20:14 +0100

    Update Action.java

--- Action.java
+++ Action.java
@@ -1,5 +1,14 @@
 package Projet.Main;
 
-public class Action {
+public class Action extends Fonds{
+	String NomAc = "Ac";
+	private float Taux=1;
+	public Action(String nom, float montant) {
+		super(nom, montant);
+	}
+	public void UpdateMontant(float montant, float Taux) {
+		this.montant = montant;
+		this.montant = montant*Taux;
+	}
 
 }
